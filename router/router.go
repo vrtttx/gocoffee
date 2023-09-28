dd64202f3413027a8275760984c1aeb73efff061
ynncstslv <ynncstslv@gmail.com>
2023-09-28 17:50:33 -0300

    feat: router logic but still only for get all

---
+++ router/router.go
@@ -0,0 +1,29 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/vrtttx/gocoffee/controllers"
+)
+
+func Routes() http.Handler {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Recoverer)
+	router.Use(cors.Handler(cors.Options {
+		AllowedOrigins: []string{"https://*", "https://*"},
+		AllowedMethods: []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: true,
+		MaxAge: 300,
+	}))
+
+	// coffee routes
+	router.Get("/api/v1/coffees", controllers.GetAllCoffees)
+
+	return router
+}
