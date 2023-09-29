089dd660f8bc8655e22ea353ca9450fca600a204
ynncstslv <ynncstslv@gmail.com>
2023-09-29 09:14:07 -0300

    feat: get by id controller

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -3,9 +3,13 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/vrtttx/gocoffee/helpers"
 	"github.com/vrtttx/gocoffee/services"
 )
+
+var models services.Models
+var coffee = models.Coffee
 
 func GetAllCoffees(w http.ResponseWriter, r *http.Request) {
 	var coffees services.Coffee
@@ -19,4 +23,17 @@ func GetAllCoffees(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helpers.WriteJSON(w, http.StatusOK, helpers.Envelope{"coffees": all})
+}
+
+func GetCoffeeById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	coffee, err := coffee.GetCoffeeById(id)
+
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+
+		return
+	}
+
+	helpers.WriteJSON(w, http.StatusOK, coffee)
 }
